9b031b9d49b3f22d2d028d532913a4d4938d7d4a
Sergey <1st_equivalent@mail.ru>
2016-12-23 23:57:26 +0300

    Настроил переключение классов для попапа (выскакивает)

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,9 @@
 "use strict";
 
-var button = document.querySelector(".btn-hotels-search-form");
+var button = document.querySelector(".btn-hotels-search"),
+    popup = document.querySelector(".index-search-hotels");
+
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.toggle("index-search-hotels-show");
+});
