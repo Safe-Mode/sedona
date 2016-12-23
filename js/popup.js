0f5677fd714ee3ac2d88d0bad6c0241f34d898a2
Sergey <1st_equivalent@mail.ru>
2016-12-24 00:32:40 +0300

    Добавил проверку формы на пустоту

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,19 @@
 "use strict";
 
 var button = document.querySelector(".btn-hotels-search"),
-    popup = document.querySelector(".index-search-hotels");
+    form = document.querySelector("form"),
+    dateOn = form.querySelector("[name=date-on]"),
+    dateOff = form.querySelector("[name=date-off]");
 
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    popup.classList.toggle("index-search-hotels-show");
+    form.classList.toggle("index-search-hotels-show");
+    dateOn.focus();
+});
+
+form.addEventListener("submit", function(event) {
+    if (!dateOn.value || !dateOff.value) {
+        event.preventDefault();
+        console.log('Введи');
+    }
 });
