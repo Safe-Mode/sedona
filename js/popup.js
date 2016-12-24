f3db64ead91ad2822d492e61ac37ba2bfb48c422
Sergey <1st_equivalent@mail.ru>
2016-12-24 19:17:33 +0300

    Настроил добавление класса modal-error

--- js/popup.js
+++ js/popup.js
@@ -9,17 +9,25 @@ button.addEventListener("click", function(event) {
     event.preventDefault();
     form.classList.toggle("index-search-hotels-show");
     dateOn.focus();
+
+    if (form.classList.contains("modal-error")) {
+        form.classList.remove("modal-error");
+    }
 });
 
 form.addEventListener("submit", function(event) {
     if (!dateOn.value || !dateOff.value) {
         event.preventDefault();
-        console.log('Введи');
+        form.classList.add("modal-error");
     }
 });
 
 window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27 && form.classList.contains("index-search-hotels-show")) {
         form.classList.remove("index-search-hotels-show");
+
+        if (form.classList.contains("modal-error")) {
+            form.classList.remove("modal-error");
+        }
     }
 });
